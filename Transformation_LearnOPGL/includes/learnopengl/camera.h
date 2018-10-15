c3b44c898a79928e889051ec7bec88bf406d5476
Matheus Hertzog <mihertzog@inf.ufpel.edu.br>
2018-10-15 00:43:33 -0300

    Add files via upload

--- Transformation_LearnOPGL/includes/learnopengl/camera.h
+++ Transformation_LearnOPGL/includes/learnopengl/camera.h
@@ -4,8 +4,7 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/rotate_vector.hpp>
-#include <glm/gtx/spline.hpp>
+
 #include <vector>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
@@ -16,30 +15,12 @@ enum Camera_Movement {
     RIGHT
 };
 
-enum Animations {
-	TRANSLATEZ,
-	TRANSLATER,
-	TRANSLATEL,
-	TRANSLATEMZ,
-	ROTATE_POINT,
-	ROTATE,
-	BSPLINE,
-	BEZIER
-};
-
-enum Anim_Orientations {
-	X, Y, Z
-};
-
 // Default camera values
 const float YAW         = -90.0f;
 const float PITCH       =  0.0f;
 const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
 const float ZOOM        =  45.0f;
-const float ROTATION_SPEED = glm::radians(60.0f) * 12.0f;
-const float STEP = 3.5f;
-
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
@@ -47,18 +28,11 @@ class Camera
 {
 public:
     // Camera Attributes
-	glm::vec3 Position;
-	glm::vec3 start_Position;
+    glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
     glm::vec3 Right;
-	glm::vec3 start_Front;
-	glm::vec3 start_Up;
-	glm::vec3 start_Right;
     glm::vec3 WorldUp;
-
-	std::vector<glm::vec3> mypath;
-
     // Euler Angles
     float Yaw;
     float Pitch;
@@ -66,14 +40,6 @@ public:
     float MovementSpeed;
     float MouseSensitivity;
     float Zoom;
-	float anim_start = 0.0;
-	float anim_end;
-	float anim_time = 2.0f;
-	float offset = 0.0f;
-	float t_spline = 0.0f;
-
-	bool anim_Started = false;
-	std::vector<Animations> animations;
 
     // Constructor with vectors
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
@@ -83,11 +49,6 @@ public:
         Yaw = yaw;
         Pitch = pitch;
         updateCameraVectors();
-		mypath.push_back(glm::vec3(0, 0, 3));
-		mypath.push_back(glm::vec3(3, 3, 0));
-		mypath.push_back(glm::vec3(0, 0, -3));
-		mypath.push_back(glm::vec3(-3, -3, 0));
-		mypath.push_back(glm::vec3(0, 0, 3));
     }
     // Constructor with scalar values
     Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
@@ -97,12 +58,6 @@ public:
         Yaw = yaw;
         Pitch = pitch;
         updateCameraVectors();
-		//path to camera do
-		mypath.push_back(glm::vec3(0, 0, 3));
-		mypath.push_back(glm::vec3(3, 3, 0));
-		mypath.push_back(glm::vec3(0, 0, -3));
-		mypath.push_back(glm::vec3(-3, -3, 0));
-		mypath.push_back(glm::vec3(0, 0, 3));
     }
 
     // Returns the view matrix calculated using Euler Angles and the LookAt Matrix
@@ -157,124 +112,8 @@ public:
         if (Zoom >= 45.0f)
             Zoom = 45.0f;
     }
-	void LookAt(glm::vec3 target) {
-		Front = glm::normalize(target - Position);
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross( Front, Right ));
-	}
-	void Animate() {
-		glm::mat4 res_m(1.0f);
-		float deltaTime;
-		float curentTime;
-		
-		unsigned short end_anim_flag = 0;
-		bool anim_ended = false;
-
-		if (anim_Started == false && animations.size() > 0) {
-			anim_Started = true;
-			anim_start = glfwGetTime();  //-offset
-			anim_end = anim_start + anim_time;
-			start_Right = Right;
-			start_Up = Up;
-			start_Front = Front;
-			start_Position = Position;
-		}
-
-		curentTime = glfwGetTime();
-		deltaTime = ((curentTime - anim_start) / (anim_end - anim_start)) + offset;
-		offset = 0.0f;
-		
-		if (curentTime >= anim_end) {
-			end_anim_flag = 1;
-			anim_ended = true;
-			deltaTime = 1.0f;
-		} 
-		printf("cur time: %.3f\n", curentTime);
-		printf("cur end: %.3f\n", anim_end);
-		printf("cur start: %.3f\n\n", anim_start);
-		//printf("delta time %.2f\n", deltaTime);
-		//printf("Position: %.3f, %.3f, %.3f\n", Position.x, Position.y, Position.z);
-		if (animations.size() > 0) {
-			switch (animations[0]) {
-			case ROTATE_POINT:
-				//res_m = rotate_about(glm::vec3(0.0f, -1.75f, 0.0f), deltaTime); //model pos 0.0f, -1.75f, 0.0f
-				res_m = rotate_about(glm::vec3(0.0f, 0.f, 0.f), deltaTime); //model pos 0.0f, -1.75f, 0.0f
-				break;
-			case ROTATE:
-				res_m = Rotate(deltaTime);
-				break;
-			case BSPLINE:
-				if (t_spline < 4);
-					B_Spline(deltaTime);
-				break;
-			case BEZIER:
-				LookAt(glm::vec3(0, 0, 0));
-				mybez(deltaTime, mypath, anim_ended);
-				break;
-			case TRANSLATEZ:
-				TranslateZ(deltaTime);
-				break;
-			case TRANSLATEMZ:
-				TranslateMZ(deltaTime);
-				break;
-			case TRANSLATER:
-				TranslateR(deltaTime);
-				break;
-			case TRANSLATEL:
-				TranslateL(deltaTime);
-				break;
-			}
-			
-			
-			
-		}
-
-		if (end_anim_flag == 1 && animations.size() > 0) {  //if animation has ended update anim queue
-			anim_Started = false;
-			animations.erase(animations.begin());
-			//if ( animations.size() < 1 )
-			anim_start = offset;
-			t_spline = 0.0f;
-			//updateDirections(res_m); //update Right ,Up , Front vectors of camera
-		}
-			
-	}
 
 private:
-	glm::vec3 mybez(float t, std::vector<glm::vec3>& path, bool ended) {
-		printf("t = %.2f\n", t);
-		if (path.size() < 2) return glm::vec3(0, 0, 0);
-
-		float c1, c2, c3;
-		glm::vec3 p;
-		bool linear = false;
-
-		c1 = pow(1 - t, 2);
-		c2 = 2 * t * (1 - t);
-		c3 = pow(t, 2);
-
-		if (path.size() > 2) { //quad
-			p = c1 * path[0] + c2 * path[1] + c3 * path[2];
-
-			//printf("Quad\n");
-		}
-		else { //linear
-			p = (1 - t)*path[0] + t * path[1];
-			printf("Linear\n");
-			linear = true;
-		}
-		if (ended == true) {
-			if (linear)
-				path.erase(path.begin(), path.end());
-			else
-				path.erase(path.begin(), path.begin() + 2);
-		}
-
-		Position = p;
-
-		return p;
-
-	}
     // Calculates the front vector from the Camera's (updated) Euler Angles
     void updateCameraVectors()
     {
@@ -288,137 +127,5 @@ private:
         Right = glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         Up    = glm::normalize(glm::cross(Right, Front));
     }
-
-
-	//model pos 0.0f, -1.75f, 0.0f
-
-	glm::mat4 rotate_about(glm::vec3 point, float t) {
-		glm::mat4 view(1.0f);
-		
-		view = glm::translate(view,  point);
-		view = glm::rotate(view, ROTATION_SPEED*t, glm::vec3(0, 1, 0));
-		view = glm::translate(view, start_Position - point);
-
-
-		Front = glm::normalize(point - Position);
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		//Up = glm::normalize(glm::cross(Front, Right));
-
-		Position.x = view[3][0];
-		Position.y = view[3][1];
-		Position.z = view[3][2];
-		
-		return view;
-	}
-	glm::mat4 Rotate(float t) {
-		glm::mat4 rot_m = getMatrix(start_Right, start_Up, start_Front, start_Position); //builds rotation matrix from camera vectors
-		
-		glm::mat4 res_m = glm::rotate(rot_m, ROTATION_SPEED * t, glm::vec3(0, 0, 1)); //rotates 
-
-		updateDirections(res_m); //updates Right,Up,Front vectors of camera
-		return res_m;
-	}
-	void B_Spline(float t) {
-		float tspline = t * 4; //normalizes deltaTime to range 0 -> n of cps
-		printf("tspline : %.2f\n", tspline);
-		std::vector<glm::vec3> cp;
-		cp.push_back(glm::vec3(0, 0, 3));
-		cp.push_back(glm::vec3(3, 3, 0));
-		cp.push_back(glm::vec3(0, 0, -3));
-		cp.push_back(glm::vec3(-3, -3, 0));
-		cp.push_back(glm::vec3(0, 0, 3));
-
-		glm::vec3 res = catmull_rom_spline(cp,tspline);
-		Position = res;
-
-		Front = glm::normalize(glm::vec3(0,0,0) - Position); //camera always face origin
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross(Front, Right));
-
-		t_spline += 0.01f;
-	}
-
-	void TranslateZ(float t) {
-		Position.z = start_Position.z + STEP * t;
-
-		Front = glm::normalize(glm::vec3(0, 0, 0) - Position); //camera always face origin
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross(Front, Right));
-	}
-	void TranslateR(float t) {
-		Position.x = start_Position.x + STEP * t;
-
-		Front = glm::normalize(glm::vec3(0, 0, 0) - Position); //camera always face origin
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross(Front, Right));
-	}
-	void TranslateL(float t) {
-		Position.x = start_Position.x - STEP * t;
-
-		Front = glm::normalize(glm::vec3(0, 0, 0) - Position); //camera always face origin
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross(Front, Right));
-	}
-	void TranslateMZ(float t) {
-		Position.z = start_Position.z - STEP * t;
-
-		Front = glm::normalize(glm::vec3(0, 0, 0) - Position); //camera always face origin
-		Right = glm::normalize(glm::cross(WorldUp, Front));
-		Up = glm::normalize(glm::cross(Front, Right));
-	}
-
-
-	glm::mat4 getMatrix(glm::vec3 right, glm::vec3 up, glm::vec3 front, glm::vec3 position) {
-		glm::mat4 mat;
-		mat[0][0] = right.x;
-		mat[0][1] = right.y;
-		mat[0][2] = right.z;
-
-		mat[1][0] = up.x;
-		mat[1][1] = up.y;
-		mat[1][2] = up.z;
-
-		mat[2][0] = front.x;
-		mat[2][1] = front.y;
-		mat[2][2] = front.z;
-
-		mat[3][0] = position.x;
-		mat[3][1] = position.y;
-		mat[3][2] = position.z;
-		return mat;
-	}
-
-	void updateDirections(glm::mat4 mat) {
-		Right.x = mat[0][0];
-		Right.y = mat[0][1];
-		Right.z = mat[0][2];
-
-		Up.x = mat[1][0];
-		Up.y = mat[1][1];
-		Up.z = mat[1][2];
-
-		Front.x = mat[2][0];
-		Front.y = mat[2][1];
-		Front.z = mat[2][2];
-
-		Position.x = mat[3][0];
-		Position.y = mat[3][1];
-		Position.z = mat[3][2];
-	}
-
-	glm::vec3 catmull_rom_spline(const std::vector<glm::vec3>& cp, float t)
-	{
-		// indices of the relevant control points
-		int i0 = (int)glm::clamp(t - 1, 0.0f, (float)cp.size() - 1);
-		int i1 = (int)glm::clamp(t, 0.0f, (float)cp.size() - 1);
-		int i2 = (int)glm::clamp(t + 1, 0.0f, (float)cp.size() - 1);
-		int i3 = (int)glm::clamp(t + 2, 0.0f, (float)cp.size() - 1);
-
-		// parameter on the local curve interval
-		float local_t = glm::fract(t);
-
-		return glm::catmullRom(cp[i0], cp[i1], cp[i2], cp[i3], local_t);
-	}
-
 };
 #endif
--- Transformation_LearnOPGL/includes/learnopengl/model.h
+++ Transformation_LearnOPGL/includes/learnopengl/model.h
@@ -5,11 +5,12 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/spline.hpp>
 #include <stb_image.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
+#include <glm/gtx/vector_angle.hpp>
 #include <learnopengl/mesh.h>
 #include <learnopengl/shader.h>
 
@@ -20,6 +21,26 @@
 #include <map>
 #include <vector>
 using namespace std;
+
+
+enum Animations {
+	ROTATEX,
+	ROTATEY,
+	ROTATEZ,
+	ROTATE_ABOUT,
+	TRANSLATE,
+	SCALE_UP, SCALE_DOWN,
+	BSPLINE,
+	BEZIER
+};
+enum Directions {
+	mFORWARD, mBACKWARD,
+	mLEFT, mRIGHT, mUP, mDOWN,
+	X, Y, Z
+};
+
+const float STEP = 0.5f;
+const float ROTATION_SPEED = glm::radians(60.0f);
 
 unsigned int TextureFromFile(const char *path, const string &directory, bool gamma = false);
 
@@ -32,12 +53,413 @@ public:
     string directory;
     bool gammaCorrection;
 
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Right;
+	glm::vec3 Up;
+	glm::vec3 start_Front;
+	glm::vec3 start_Right;
+	glm::vec3 start_Up;
+	glm::vec3 start_Position;
+	glm::vec3 WorldUp;
+	glm::mat4 Matrix;
+
+	std::vector<glm::vec3> mypath;
+	glm::vec3 radius = glm::vec3(2, 0, 0);
+
+	float anim_start = 0.0;
+	float anim_end;
+	float anim_time = 2.0f;
+	float t_spline = 0.0f;
+	float scale;
+	float start_scale;
+	float offset = 0.0f;
+
+	float angleX = 0.0f;
+	float angleY = 0.0f;
+	float angleZ = 0.0f;
+
+	bool anim_Started = false;
+
+	std::vector<Animations> animations;
+	std::vector<Directions> directions;
+	/*  Animations    */
+	void Animate() {
+		glm::mat4 res(1.0f);
+		float deltaTime, currentTime;
+		bool anim_ended = false;
+
+		if (anim_Started == false && animations.size() > 0) {
+			anim_Started = true;
+			anim_start = glfwGetTime();
+			anim_end = anim_start + anim_time;
+			start_Right = Right;
+			start_Up = Up;
+			start_Front = Front;
+			start_Position = Position;
+			start_scale = scale;
+		}
+
+		currentTime = glfwGetTime();
+		deltaTime = ( (currentTime - anim_start) / (anim_end - anim_start) ) + offset;
+		offset = 0.0f;
+
+		if (currentTime >= anim_end) {
+			anim_ended = true;
+			offset = (currentTime - anim_end);
+			deltaTime = deltaTime - offset;
+		}
+
+		if (animations.size() > 0) {
+			switch (animations[0])
+			{
+			case ROTATEX:
+				rotateX(deltaTime, anim_ended);
+				break;
+			case ROTATEY:
+				rotateY(deltaTime, anim_ended);
+				break;
+			case ROTATEZ:
+				rotateZ(deltaTime, anim_ended);
+				break;
+			case ROTATE_ABOUT:
+				rotate_about(deltaTime, glm::vec3(0, 0, 0), anim_ended);
+				break;
+			case TRANSLATE:
+				if(directions.size() > 0)
+					translate(deltaTime, directions[0], anim_ended);
+				break;
+			case BSPLINE:
+				catmull(deltaTime, anim_ended);
+				break;
+			case BEZIER:
+				//bezier(deltaTime, anim_ended);
+				mybez(deltaTime, mypath, anim_ended);
+				break;
+			case SCALE_UP:
+				scale_model_UP(deltaTime, anim_ended);
+				break;
+			case SCALE_DOWN:
+				scale_model_DOWN(deltaTime, anim_ended);
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (anim_ended == true) {
+			anim_Started = false;
+			if(animations.size() > 0)
+				animations.erase(animations.begin());
+			if(directions.size() > 0)
+				directions.erase(directions.begin());
+			anim_start = offset;
+		}
+
+	}
+	void scale_model_UP(float deltaTime, bool ended) {
+		float new_scale;
+		new_scale = start_scale + (deltaTime*0.1);
+			
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(new_scale, new_scale, new_scale));
+		glm::mat4 model_m = getMatrix(Right, Up, Front, Position);
+
+		Matrix = model_m * scale_m;
+
+		if (ended == true) scale = new_scale;
+	}
+	void scale_model_DOWN(float deltaTime, bool ended) {
+		float new_scale;
+		new_scale = start_scale - deltaTime * 0.1;
+
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(new_scale, new_scale, new_scale));
+		glm::mat4 model_m = getMatrix(Right, Up, Front, Position);
+
+		Matrix = model_m * scale_m;
+
+		if (ended == true) scale = new_scale;
+	}
+	void bezier(float deltaTime, bool ended) {
+		glm::vec2 points[3];
+		points[0].x = start_Position.x; points[0].y = start_Position.y;
+		points[1] = glm::vec2(start_Position.x, 5);
+		points[2] = glm::vec2(start_Position.x + 3, 0);
+		glm::vec2 ans = getBezierPoint(points,3, deltaTime);
+		Matrix[3][0] = ans[0];
+		Matrix[3][1] = ans[1];
+		if (ended == true) {
+			Position.x = ans.x;
+			Position.y = ans.y;
+		}
+	}
+	glm::vec2 getBezierPoint(glm::vec2* points, int numPoints, float t) {
+		glm::vec2* tmp = new glm::vec2[numPoints];
+		memcpy(tmp, points, numPoints * sizeof(glm::vec2));
+		int i = numPoints - 1;
+		while (i > 0) {
+			for (int k = 0; k < i; k++)
+				tmp[k] = tmp[k] + t * (tmp[k + 1] - tmp[k]);
+			i--;
+		}
+		glm::vec2 answer = tmp[0];
+		delete[] tmp;
+		return answer;
+	}
+	glm::vec3 mybez(float t, std::vector<glm::vec3>& path, bool ended) {
+		if (path.size() < 2) return glm::vec3(0, 0, 0);
+
+		float c1, c2, c3;
+		glm::vec3 p;
+		bool linear = false;
+
+		c1 = pow(1 - t, 2);
+		c2 = 2 * t * (1 - t);
+		c3 = pow(t, 2);
+
+		if (path.size() > 2) { //quad
+			p = c1 * path[0] + c2 * path[1] + c3 * path[2];
+			
+			printf("Quad\n");
+		}
+		else { //linear
+			p = (1 - t)*path[0] + t * path[1];
+			printf("Linear\n");
+			linear = true;
+		}
+		if (ended == true) { 
+			if(linear) 
+				path.erase(path.begin(), path.end());
+			else
+				path.erase(path.begin(), path.begin() + 2);
+		}
+
+		Matrix[3][0] = p.x;
+		Matrix[3][1] = p.y;
+		Matrix[3][2] = p.z;
+
+		return p;
+
+	}
+	void catmull(float deltaTime, bool ended) {
+		
+		std::vector<glm::vec3> cp;
+		cp.push_back(start_Position);
+		cp.push_back(start_Position + glm::vec3(0.1,1.2,0));
+		cp.push_back(start_Position + glm::vec3(1.2, 1.4, 0));
+		cp.push_back(start_Position + glm::vec3(1.3, 0, 0));
+
+		float t = deltaTime * (mypath.size() - 1);
+
+		glm::vec3 ans = catmull_rom_spline(mypath, t);
+		if (ended == true) Position = ans;
+		Matrix[3][0] = ans.x;
+		Matrix[3][1] = ans.y;
+		Matrix[3][2] = ans.z;
+	}
+	// Rotates object around point p
+	void rotate_about(float deltaTime, glm::vec3 p, bool ended) {
+
+		glm::vec3 axis = glm::vec3(0, 1, 0); //rotation axis
+
+		glm::mat4 model = getMatrix(Right,Up,Front, glm::vec3(0,0,0));
+
+		model = glm::translate(model, p);
+
+		float new_angle = angleX + (6.28f * deltaTime);
+
+		if (new_angle >= 6.28) new_angle -= 6.28f;
+
+		model = glm::rotate(model, new_angle, axis);
+
+		model = glm::translate(model, -p);
+		model = glm::translate(model, Position);
+
+		model = glm::scale(model, glm::vec3(scale));
+
+		/*  look at point p  */                  /*
+		glm::vec3 look_direction = Position - p;
+		glm::vec3 front = glm::vec3(model[2]);
+		float angle = glm::angle(front, look_direction);
+
+		model = glm::rotate(model, - angle, axis);*/
+
+		if (ended == true) {
+			//Position = glm::vec3(Matrix[3]);
+			angleX = new_angle;
+		}
+
+		
+		Matrix = model;
+	}
+
+	void rotateX(float deltaTime, bool ended) {
+		glm::vec3 axis;
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(scale));
+		glm::mat4 rotation = getMatrix(start_Right,start_Up,start_Front,start_Position);
+
+		axis = glm::vec3(1, 0, 0);
+
+		rotation = glm::rotate(rotation, ROTATION_SPEED * deltaTime, axis);
+
+		//saves vectors and position if its last iteration
+		if (ended == true) updateVectors(rotation);
+
+		Matrix = rotation * scale_m;
+		return;
+	}
+	void rotateY(float deltaTime, bool ended) {
+		glm::vec3 axis;
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(scale));
+		glm::mat4 rotation = getMatrix(start_Right, start_Up, start_Front, start_Position);
+
+		axis = glm::vec3(0, 1, 0);
+		rotation = glm::rotate(rotation, ROTATION_SPEED * deltaTime, axis);
+
+		//saves vectors and position if its last iteration
+		if (ended == true) updateVectors(rotation);
+
+		Matrix = rotation * scale_m;
+		return;
+	}
+	void rotateZ(float deltaTime, bool ended) {
+		glm::vec3 axis;
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(scale));
+		glm::mat4 rotation = getMatrix(start_Right, start_Up, start_Front, start_Position);
+		axis = glm::vec3(0, 0, 1);
+
+		rotation = glm::rotate(rotation, ROTATION_SPEED * deltaTime, axis);
+
+		//saves vectors and position if its last iteration
+		if (ended == true) updateVectors(rotation);
+
+		Matrix = rotation * scale_m;
+		return;
+	}
+
+	void translate(float deltaTime, Directions dir, bool ended) {
+		
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, scale));
+		glm::mat4 model_matrix = getMatrix(start_Right, start_Up, start_Front, start_Position);
+		glm::vec3 tmp = start_Position;
+		
+		if (dir == mFORWARD)
+			tmp.z += STEP * deltaTime;
+		if (dir == mBACKWARD)
+			tmp.z -= STEP * deltaTime;
+		if (dir == mRIGHT)
+			tmp.x += STEP * deltaTime;
+		if (dir == mLEFT)
+			tmp.x -= STEP * deltaTime;
+		if (dir == mUP)
+			tmp.y += STEP * deltaTime;
+		if (dir == mDOWN)
+			tmp.y -= STEP * deltaTime;
+
+		model_matrix[3][0] = tmp.x;
+		model_matrix[3][1] = tmp.y;
+		model_matrix[3][2] = tmp.z;
+
+		if (ended == true) Position = tmp;
+
+		Matrix = model_matrix * scale_m;
+		return;
+	}
+
+	glm::vec3 catmull_rom_spline(const std::vector<glm::vec3>& cp, float t)
+	{
+		// indices of the relevant control points
+		int i0 = (int)glm::clamp(t - 1, 0.0f, (float)cp.size() - 1);
+		int i1 = (int)glm::clamp(t, 0.0f, (float)cp.size() - 1);
+		int i2 = (int)glm::clamp(t + 1, 0.0f, (float)cp.size() - 1);
+		int i3 = (int)glm::clamp(t + 2, 0.0f, (float)cp.size() - 1);
+
+		// parameter on the local curve interval
+		float local_t = glm::fract(t);
+
+		return glm::catmullRom(cp[i0], cp[i1], cp[i2], cp[i3], local_t);
+	}
+
+	glm::mat4 getMatrix(glm::vec3 right, glm::vec3 up, glm::vec3 front, glm::vec3 position) {
+		glm::mat4 mat;
+		mat[0][0] = right.x;
+		mat[0][1] = right.y;
+		mat[0][2] = right.z;
+
+		mat[1][0] = up.x;
+		mat[1][1] = up.y;
+		mat[1][2] = up.z;
+
+		mat[2][0] = front.x;
+		mat[2][1] = front.y;
+		mat[2][2] = front.z;
+
+		mat[3][0] = position.x;
+		mat[3][1] = position.y;
+		mat[3][2] = position.z;
+
+		return mat;
+	}
+
+	void updateDirections(glm::mat4 mat) {
+		glm::vec3 front(mat[2][0], mat[2][1], mat[3][1]);
+		Front = glm::normalize(front);
+		Right = glm::normalize(glm::cross(WorldUp, Front));
+		Up = glm::normalize(glm::cross(Right, Front));
+		Position = glm::vec3(mat[3][0], mat[3][1], mat[3][2]);
+		
+		Right.x = mat[0][0];
+		Right.y = mat[0][1];
+		Right.z = mat[0][2];
+
+		Up.x = mat[1][0];
+		Up.y = mat[1][1];
+		Up.z = mat[1][2];
+
+		Front.x = mat[2][0];
+		Front.y = mat[2][1];
+		Front.z = mat[2][2];
+
+		Position.x = mat[3][0];
+		Position.y = mat[3][1];
+		Position.z = mat[3][2];
+	}
+
+	void updateVectors(glm::mat4 m) {
+		Right = glm::vec3(m[0][0], m[0][1], m[0][2]);
+		Up = glm::vec3(m[1][0], m[1][1], m[1][2]);
+		Front = glm::vec3(m[2][0], m[2][1], m[2][2]);
+	}
+
     /*  Functions   */
     // constructor, expects a filepath to a 3D model.
-    Model(string const &path, bool gamma = false) : gammaCorrection(gamma)
+    Model(string const &path, bool gamma = false, glm::vec3 pos = glm::vec3(0,0,0), glm::vec3 mscale = glm::vec3(1,1,1)) : gammaCorrection(gamma)
     {
         loadModel(path);
+		Position = pos;
+		Front = glm::vec3(0., 0., 1.);
+		Up = glm::vec3(0., 1., 0.);
+		Right = glm::normalize(glm::cross(Up, Front));
+		Matrix = getMatrix(Right, Up, Front, Position);
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), mscale);
+		Matrix = Matrix * scale_m;
     }
+	Model(string const &path,glm::vec3 pos = glm::vec3(0, 0, 0), glm::vec3 mscale = glm::vec3(1, 1, 1))
+	{
+		loadModel(path);
+		Position = pos;
+		Front = glm::vec3(0., 0., 1.);
+		Up = glm::vec3(0., 1., 0.);
+		Right = glm::normalize(glm::cross(Up, Front));
+		scale = mscale.x;
+		Matrix = getMatrix(Right, Up, Front, Position);
+		glm::mat4 scale_m = glm::scale(glm::mat4(1.0f), mscale);
+		Matrix = Matrix * scale_m;
+		mypath.push_back(glm::vec3(0, 0, 3));
+		mypath.push_back(glm::vec3(3, 3, 0));
+		mypath.push_back(glm::vec3(0, 0, -3));
+		mypath.push_back(glm::vec3(-3, -3, 0));
+		mypath.push_back(glm::vec3(0, 0, 3));
+	}
 
     // draws the model, and thus all its meshes
     void Draw(Shader shader)
@@ -240,4 +662,5 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
 
     return textureID;
 }
+
 #endif
